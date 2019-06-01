164a201c570261f5464e81334d9902c15aae42a6
Jens Grochtdreis <jens@grochtdreis.de>
2019-06-01 13:44:39 +0200

    Import-Versuche in der main.js auskommentiert

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,2 @@
-@import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-@import "../../node_modules/bootstrap/dist/bootstrap.min.js";
+// @import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+// @import "../../node_modules/bootstrap/dist/bootstrap.min.js";
