b2dc195c4a4704503786e752fe5c36500209d8ac
Jens Grochtdreis <jens@grochtdreis.de>
2019-06-01 14:04:05 +0200

    Ein neues Modul sowie JS-Dateien hinzugefügt

--- data.js
+++ data.js
@@ -3,7 +3,9 @@ module.exports = {
 		'/styles/main.css'
 	],
 	scripts: [
-		'/scripts/main.js'
+		// '/scripts/main.js'
+		'/scripts/bootstrap.bundle.min.js',
+		'/scripts/jquery.min.js'
 		// './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 	]
 }
